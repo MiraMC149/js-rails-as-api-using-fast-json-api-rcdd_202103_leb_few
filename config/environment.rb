81318638e0ca920ef81651a941bec4f1a07d6768
Mira Chami <miramc149@gmail.com>
2021-05-16 20:21:47 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -6,3 +6,10 @@ Rails.application.initialize!
 rails g serializer Bird
 rails g serializer Location
 rails g serializer Sighting
+
+class SightingSerializer
+  include FastJsonapi::ObjectSerializer
+  attributes :created_at
+  belongs_to :bird
+  belongs_to :location
+end
